2ae1e3a10e3919c184223647f5c1932e49926ab7
aliquijada <110144836+aliquijada@users.noreply.github.com>
2024-08-14 17:26:35 +0100

    Update Selectores.js

--- Selectores.js
+++ Selectores.js
@@ -1,28 +1,29 @@
 //Clase para manejar metodos de fecha dando que se selecciona un agno
 
 
-exports.getDateList =  function(year) {
-  //retorna una lista de strings con la fecha en formato DD/MM/AAAA
+exports.getDateList = function(year, disp_year) {
   var dates = [];
-  var startDate = new Date(year, 0, 1); 
-  var endDate = new Date(year, 11, 31); 
+  if (year !== null) {
+    var startDate = new Date(year, 0, 1); 
+    var endDate = new Date(year, 11, 31); 
 
-  for (var date = startDate; date <= endDate; date.setDate(date.getDate() + 1)) {
-    
-    var day = ("0" + date.getDate()).slice(-2);
-    var month = ("0" + (date.getMonth() + 1)).slice(-2);
-    var formattedDate = day + "/" + month + "/" + date.getFullYear();
-    dates.push(formattedDate);
-    
+    for (var date = startDate; date <= endDate; date.setDate(date.getDate() + 1)) {
+      var day = ("0" + date.getDate()).slice(-2);
+      var month = ("0" + (date.getMonth() + 1)).slice(-2);
+      var formattedDate = day + "/" + month + "/" + date.getFullYear();
+      dates.push(formattedDate);
+    }
   }
-  return dates
-}
+
+  return dates;
+};
+
 
 //si las bandas de los tiff se llaman YYYY-MM-DD entonces esta funcion no es necesaria
 exports.getBand = function(date, dateList){
-  var indexBand = dateList.indexOf(date)
-  return indexBand
-}
+  var indexBand = dateList.indexOf(date);
+  return indexBand;
+};
 
 
 
@@ -47,28 +48,28 @@ exports.checkFecha = function(dateString) {
     
 
     if(isNaN(dia) || parseInt(dia)>31){
-      print('formato incorrecto porque puse mas de 31 en los dias')
-      return 'Formato incorrecto de fecha'
+      print('formato incorrecto porque puse mas de 31 en los dias');
+      return 'Formato incorrecto de fecha';
     }
     // verificar bisiesto
     if(mes == '02' && !bisiesto(agno) && dia>28){
       //print('no existe la fecha ',fechaEspecifica,' .No es agno bisisesto');
-      return 'Verifique la fecha'
+      return 'Verifique la fecha';
     }
     //verificar meses con menos de 31 dias
     if (dia>30 && meses30.indexOf(mes) !== -1){
       //print(mes, ' solo tiene 30 dias');
-      return 'Verifique la fecha'
+      return 'Verifique la fecha';
     }
     
     //verificar que agno sea posterior a 2015
     if(agno<2015){
-      return 'Datos disponibles a partir del año 2015'
+      return 'Datos disponibles a partir del año 2015';
     }
     
     //verificar que agno sea anterior a 2025
     if(agno>2024){
-      return 'Verifique la fecha'
+      return 'Verifique la fecha';
     }
     
     print(fechaEspecifica);
